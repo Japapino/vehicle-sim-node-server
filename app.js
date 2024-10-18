c24027775623d9572992f5d66eaf7f6b92fae73f
Stefan M <stefan.murakami@gmail.com>
2024-10-18 12:25:16 -0400

    pulls vehicle data

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require("express");
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const config = require('./webpack.config.js');
+const webpack = require("webpack");
+const webpackDevMiddleware = require("webpack-dev-middleware");
+const webpackHotMiddleware = require("webpack-hot-middleware");
+const config = require("./webpack.config.js");
 const compiler = webpack(config);
-const cors = require('cors');
-const puppeteer = require('puppeteer-core')
+const cors = require("cors");
+const puppeteer = require("puppeteer-core");
 
 const app = express();
 const PORT = 3000;
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
 
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
 
 app.use(
@@ -57,62 +57,78 @@ app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-app.get("/hello", function(req, res) {
+app.get("/hello", function (req, res) {
   res.send({
-    hello: "Hello World"
+    hello: "Hello World",
   });
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/public/page.html');
+  res.sendFile(__dirname + "/public/page.html");
 });
 
-(async () => {
+async () => {
   const browser = await puppeteer.launch({
-    executablePath: "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
-  }); 
-  const page = await browser.newPage(); 
+    executablePath:
+      "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe",
+  });
+  const page = await browser.newPage();
 
-  await page.goto('https://www.youtube.com/feed/trending');
+  await page.goto("https://www.youtube.com/feed/trending");
 
   const youtubeTrending = await page.evaluate(() => {
-    const trendingElements = document.querySelectorAll('#video-title');
+    const trendingElements = document.querySelectorAll("#video-title");
 
     return Array.from(trendingElements).map((element) => {
       const link = element.href;
       return { link };
-    })
+    });
   });
 
-  await browser.close(); 
+  await browser.close();
 
   app.get("/trending", (req, res) => {
-
-    res.send(youtubeTrending); 
+    res.send(youtubeTrending);
   });
-  
-});
+};
 
 (async () => {
   const browser = await puppeteer.launch({
-    executablePath: "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe",
-    headless: false, // Run in non-headless mode for debugging
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  }); 
-  const page = await browser.newPage(); 
+    executablePath:
+      "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe",
+    headless: false, 
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+  const page = await browser.newPage();
 
-  await page.goto('https://www.edmunds.com/', { waitUntil: 'networkidle2', timeout: 60000 }); // Increase timeout and wait for network to be idle
+  const link =
+    "https://www.edmunds.com/inventory/srp.html?year=2012-2012&make=honda&model=honda%7Ccivic&trim=civic%7Cex";
 
-  const elements = await page.content(); 
+  await page.goto(link, { waitUntil: "networkidle2", timeout: 60000 }); // Increase timeout and wait for network to be idle
 
-  await browser.close(); 
+  // const elements = await page.content();
+  const data = await page.evaluate(() => {
+    const scriptTag = document.querySelector(
+      'script[type="application/ld+json"'
+    ); // Adjust the selector as needed
+    return scriptTag ? scriptTag.innerText : null;
+  });
+
+  const filteredStringData = (data) => {
+    const jsonData = JSON.parse(data);
+    const filteredData = jsonData.filter(item => item['@type'] === 'Vehicle');
+    return filteredData;
+  };
+
+  console.log("filtered data: ", typeof data);
+
+  await browser.close();
 
   app.get("/estimate", (req, res) => {
-    console.log('done: ', elements); 
-    res.send(elements); 
+    res.send(filteredStringData(data));
   });
 
   app.listen(8080, () => {
-    console.log('Server is running on http://localhost:3000');
+    console.log("Server is running on http://localhost:3000");
   });
 })();
